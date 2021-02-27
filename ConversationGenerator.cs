69ddd77207be07c64fc56596378b0bec851d8267
Norman Calhoun <shakauluz73@yahoo.com>
2021-02-27 14:47:18 -0500

    funny

---
+++ ConversationGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+
+
+namespace KanyeAndRon
+{
+    class ConversationGenerator
+    {
+        public static void KanyeTalks()
+        {
+            string kanyeURL = "https://api.kanye.rest";
+            var client1 = new HttpClient();
+            var kanyeResponse = client1.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine($"Kanye says: {kanyeQuote}");
+            Console.WriteLine("_________________________________________________");
+        }
+
+
+
+        public static void RonTalks()
+        {
+            string ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var client2 = new HttpClient();
+            var ronResponse = client2.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            
+
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine($"Ron says: {ronQuote}");
+            Console.WriteLine("_________________________________________________");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A converstaion bewteen Kanye West and Ron Swanson might go something like this:");
+
+
+            for (int i = 0; i <= 5; i++) 
+            {
+                ConversationGenerator.RonTalks();
+
+                ConversationGenerator.KanyeTalks();
+            }
+        }
+    }
+}
